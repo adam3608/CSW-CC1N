b1c2a1eefda289cd24d0a2bdeeba1ac8f378cbd0
Adam Coresma de Carvalho <adamcdecarvalho@gmail.com>
2025-05-31 23:28:52 -0300

    Add files via upload

---
+++ estudo baseado na aula 20/js.js
@@ -0,0 +1,7 @@
+function resultadooo(){
+    var nome = (document.getElementById("nome").value);
+    var resultado = (document.getElementById("resultado"));
+    
+        resultado.textContent = "Ola " + nome;
+    
+}
